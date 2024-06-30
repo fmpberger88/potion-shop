290af7e9e4d130aa4c79be40a62b7d656671824f
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-30 21:44:01 +0200

    implement passport

---
+++ middlewares/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+passport.use(new LocalStrategy(
+    // set email as usernameField
+    {
+        usernameField: 'email',
+        passwordField: 'password',
+    },
+    async (email, password, done) => {
+        try {
+            const user = await User.findOne({ email: email });
+            if (!user) {
+                return done(null, false, { message: 'User not found' });
+            }
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return done(null, false, { message: 'Incorrect password' });
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    }
+));
+
+passport.session((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
+
+module.exports = passport;
