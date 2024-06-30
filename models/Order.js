d1366a307e1753e5083581439742dade17047993
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-30 21:30:27 +0200

    add Order Model

---
+++ models/Order.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User',
+        required: true
+    },
+    products: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId, ref: 'Product'
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        }
+    }],
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+    orderDate: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        default: 'pending',
+    }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
