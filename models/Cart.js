5c7dbdb728b662f7e7eb2081b52751ee4f6743a7
fmpberger88 <fabian.berger@protonmail.ch>
2024-07-03 07:04:09 +0200

    adding timestamp etc.

--- models/Cart.js
+++ models/Cart.js
@@ -5,15 +5,20 @@ const cartSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, ref: 'User',
         required: true
     },
-    products: [{
+    items: [{
         product: {
-            type: mongoose.Schema.Types.ObjectId, ref: 'Product'
+            type: mongoose.Schema.Types.ObjectId, ref: 'Product',
+            required: true
         },
         quantity: {
             type: Number,
             required: true,
+            min: 1
         }
     }]
-});
+},
+    {
+        timestamps: true
+    });
 
 module.exports = mongoose.model('Cart', cartSchema);
--- models/Order.js
+++ models/Order.js
@@ -5,16 +5,18 @@ const orderSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, ref: 'User',
         required: true
     },
-    products: [{
+    items: [{
         product: {
-            type: mongoose.Schema.Types.ObjectId, ref: 'Product'
+            type: mongoose.Schema.Types.ObjectId, ref: 'Product',
+            required: true
         },
         quantity: {
             type: Number,
             required: true,
+            min: 1
         }
     }],
-    totalAmount: {
+    total: {
         type: Number,
         required: true,
     },
@@ -26,6 +28,8 @@ const orderSchema = new mongoose.Schema({
         type: String,
         default: 'pending',
     }
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('Order', orderSchema);
