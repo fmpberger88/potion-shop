cea3cb24d8344e663bb0c15a60233d3fe77568a3
fmpberger88 <fabian.berger@protonmail.ch>
2024-07-04 07:50:57 +0200

    adding basic cart Routes

---
+++ routes/cartRoutes.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const Cart = require('../models/Cart');
+const Product = require('../models/Product');
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: false });
+
+const cartRouter = express.Router();
+
+// View Cart
+cartRouter.get('/', ensureAuthenticated, csrfProtection, async (req, res, next) => {
+    try {
+        const cart = await Cart.findOne({ user: req.user._id }).populate('items.product');
+        res.render('cart', {
+            cart: cart,
+            csrfToken: req.csrfToken(),
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Add to Cart
+cartRouter.post('/add/:productId', ensureAuthenticated, csrfProtection, async (req, res, next) => {
+    try {
+
+
+        const product = await Product.findById(req.params.productId);
+        if (!product) {
+            return res.status(404).render('error', { message: 'Product not found' });
+        }
+
+        let cart = await Cart.findOne({ user: req.user._id });
+        if (!cart) {
+            cart = new Cart({
+                user: req.user._id,
+                items: []
+            });
+        }
+
+        const existingItemIndex = cart.items.findIndex(item => item.product.equals(product._id));
+        if (existingItemIndex >= 0) {
+            cart.items[existingItemIndex].quantity += parseInt(req.body.quantity, 10);
+        } else {
+            cart.items.push({ product: product._id, quantity: parseInt(req.body.quantity, 10) });
+        }
+
+        await cart.save();
+        res.redirect('/cart');
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Remove from Cart
+cartRouter.post('/remove/:productId', ensureAuthenticated, csrfProtection, async (req, res, next) => {
+    try {
+        const cart = await Cart.findOne({ user: req.user._id });
+        if (!cart) {
+            return res.status(404).render('error', { message: 'Cart not found' });
+        }
+
+        cart.items = cart.items.filter(item => !item.product.equals(req.params.productId));
+        await cart.save();
+        res.redirect('/cart');
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = cartRouter;
