3923943b1bc1f20ab4f0b7f3bcdd0feb434b8d14
fmpberger88 <fabian.berger@protonmail.ch>
2024-07-04 07:53:16 +0200

    debug passport.js serializing

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -26,16 +26,16 @@ passport.use(new LocalStrategy(
     }
 ));
 
-passport.session((user, done) => {
+passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser(async (id, done) => {
+passport.deserializeUser( async (id, done) => {
     try {
         const user = await User.findById(id);
         done(null, user);
-    } catch (err) {
-        done(err);
+    } catch(err) {
+        done(new Error('Authentication failed'));
     }
 });
 
